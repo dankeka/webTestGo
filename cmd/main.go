64f515738fe163e185df4d5a21496714579d7206
dankeka <dankeka@inbox.ru>
2021-03-20 20:24:18 +1200

    Надеюсь что точно закончил README + немного изменил cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	serv "github.com/dankeka/webTestGo"
 	"github.com/dankeka/webTestGo/pkg/handler"
@@ -13,7 +18,21 @@ func main() {
 
 	srv := new(serv.Server)
 
-	if err := srv.Run("8080", handlers.InitRouters()); err != nil {
-		log.Fatal("error server: ", err.Error())
+	go func() {
+		if err := srv.Run("8080", handlers.InitRouters()); err != nil {
+			log.Fatal("error server: ", err.Error())
+		}
+	}()
+
+	fmt.Println("Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<- quit
+
+	fmt.Println("Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		fmt.Printf("error %s", err.Error())
 	}
 }
